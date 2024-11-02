c29516e8c631a416e35911eccc7e05cbe408047e
thomasspina <thomas@thomasspina.com>
2024-11-02 14:30:45 -0400

    so credentials need to be set with aws configure

--- src/chatbot_frontend.py
+++ src/chatbot_frontend.py
@@ -1,27 +1,18 @@
-# Source:Below code is provided by Streamlit and AWS 
-
-#1 import streamlit and chatbot file
 import streamlit as st 
-import  chatbot_backend as demo  #**Import your Chatbot file as demo
+import  chatbot_backend as demo 
 
-#2 Set Title for Chatbot - https://docs.streamlit.io/library/api-reference/text/st.title
-st.title("Hi, This is Chatbot Mauricio :sunglasses:") # **Modify this based on the title you want in want
+st.title("Hi, This is Chatbot Mauricio :sunglasses:")
 
-#3 LangChain memory to the session cache - Session State - https://docs.streamlit.io/library/api-reference/session-state
 if 'memory' not in st.session_state: 
-    st.session_state.memory = demo.demo_memory() #** Modify the import and memory function() attributes initialize the memory
+    st.session_state.memory = demo.demo_memory()
 
-#4 Add the UI chat history to the session cache - Session State - https://docs.streamlit.io/library/api-reference/session-state
-if 'chat_history' not in st.session_state: #see if the chat history hasn't been created yet
-    st.session_state.chat_history = [] #initialize the chat history
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history = []
 
-#5 Re-render the chat history (Streamlit re-runs this script, so need this to preserve previous chat messages)
 for message in st.session_state.chat_history: 
     with st.chat_message(message["role"]): 
         st.markdown(message["text"]) 
-
-#6 Enter the details for chatbot input box 
-     
+  
 input_text = st.chat_input("Powered by Bedrock and Claude") # **display a chat input box
 if input_text: 
     
