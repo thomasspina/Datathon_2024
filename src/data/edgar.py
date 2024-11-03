2b8a2dc44fde3a4bdd9e1b1c6bb7600ff5d1acc3
thomasspina <thomas@thomasspina.com>
2024-11-03 13:04:47 -0500

    yessir

--- src/data/edgar.py
+++ src/data/edgar.py
@@ -1,17 +1,35 @@
-
+#from config.settings import *
+AWS_ACCESS_KEY_ID = "AKIA3BNTGPX65AXAVA6U"
+AWS_SECRET_ACCESS_KEY = "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"
+AWS_DEFAULT_REGION = "us-west-2"
+AGENT_ID = "OBDOASWNGT"
+AGENT_ALIAS_ID = "6RSQRHKCPT"
+SEC_BUCKET_NAME = "secfedgar-source"
 from sec_edgar_downloader import Downloader
 import boto3
 import os
+import shutil
 
-def upload_directory_to_s3(local_directory, bucket_name, s3_directory):
+def download_recent_sec_directory_to_s3(stock, limit=2):
+    dl = Downloader("Datathon2024", "datathon@polymtl.ca")
+    dl.get("DEF 14A", stock, limit=limit, download_details=True)
+    dl.get("SC 13G", stock, limit=limit, download_details=True)
+    dl.get("SC 13D", stock, limit=limit, download_details=True)
+    dl.get("10-K", stock, limit=limit, download_details=True)
+    dl.get("8-K", stock, limit=2*limit, download_details=True)
+
+    upload_sec_directory_to_s3(stock, f"{stock}")
+
+# uploads the files to S3 and cleans up the local directory 
+def upload_sec_directory_to_s3(stock, s3_directory):
+    local_directory = f'./sec-edgar-filings/{stock}'
+
     s3 = boto3.resource(
         service_name='s3',
-        region_name="us-west-2",
-        aws_access_key_id="AKIA3BNTGPX65AXAVA6U",
-        aws_secret_access_key="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"
+        region_name=AWS_DEFAULT_REGION,
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
     )
-
-    
 
     for root, _, files in os.walk(local_directory):
         for file in files:
@@ -22,34 +40,15 @@ def upload_directory_to_s3(local_directory, bucket_name, s3_directory):
                 s3_file_path = os.path.join(s3_directory, relative_path)  # Ensure correct path format for S3
 
                 try:
-                    s3.Bucket(bucket_name).upload_file(local_file_path, s3_file_path)
-                    print(f"Uploaded {local_file_path} to s3://{bucket_name}/{s3_file_path}")
+                    s3.Bucket(SEC_BUCKET_NAME).upload_file(local_file_path, s3_file_path)
+                    print(f"Uploaded {local_file_path} to s3://{SEC_BUCKET_NAME}/{s3_file_path}")
                 except Exception as e:
                     print(f"Failed to upload {local_file_path}: {e}")
+        
+    shutil.rmtree('./sec-edgar-filings')
 
 
-dl = Downloader("MyCompanyName", "my.email@domain.com")
 
-# step 1: get relavant SEC filings
-stock = "AAPL"
-# dl.get("DEF 14A", stock, limit=2, download_details=True)
-# dl.get("SC 13G", stock, limit=2, download_details=True)
-# dl.get("SC 13D", stock, limit=2, download_details=True)
-# dl.get("10-K", stock, limit=3, download_details=True)
-# dl.get("8-K", stock, limit=3, download_details=True)
-
-local_directory = f'./sec-edgar-filings/{stock}'
-bucket_name = 'secfedgar-source'
-s3_directory = f'{stock}'
-
-# session = boto3.Session(
-#     aws_access_key_id="AKIA3BNTGPX65AXAVA6U",
-#     aws_secret_access_key="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",
-#     region_name="us-west-2"
-# )
-
-upload_directory_to_s3(local_directory, bucket_name, s3_directory)
-
-
+download_recent_sec_directory_to_s3("TSLA")
 
 
