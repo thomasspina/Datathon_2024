c9ad05a50c839f8e89992a4c03f43722b1cc05ed
salomemarie <salome.marie@polymtl.ca>
2024-11-03 14:54:05 -0500

    idk what i did but it works

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from src.visualization import company
 
 
 class MainPage():
-    def __init__(self):
-        self.init_state()
-        st.set_page_config(page_title="Stock Analysis Tool", layout="wide")
 
+    def __init__(self):
+        st.set_page_config(page_title="Stock Analysis Tool", layout="wide")
+        st.session_state.page = Page.STOCK_ANALYSIS
         st.sidebar.title("History")
+
+        self.init_state()
 
         for symbol, company in st.session_state.history:
             if st.sidebar.button(company):
@@ -16,13 +18,23 @@ class MainPage():
     
     def init_state(self):
         """Initialize session state variables"""
-        st.session_state.selected = "NA.TO"
         st.session_state.history = [("AMZN", "Amazon"), ("NA.TO", "Banque Nationale")]
+
+        # go to where user left off
+        if st.session_state.get("selected"):
+            self.navigate(st.session_state.selected)
+        if st.session_state.page == Page.CHAT_BOX:
+            self.company.navigate_to_chat()
+
 
     
     def navigate(self, symbol):
         st.session_state.selected = symbol
         company.CompanyPage(symbol)
+
+class Page:
+    STOCK_ANALYSIS = "stock analysis"
+    CHAT_BOX = "chat box"
 
 if __name__ == "__main__":
     MainPage()
--- src/visualization/chat.py
+++ src/visualization/chat.py
@@ -1,41 +1,36 @@
-"""import streamlit as st
+import streamlit as st
+import src.models.bedrock_agent as bedrock_agent
 
-class CompanyPage():
+class ChatBox():
     def __init__(self, symbol):
 
-        st.title("📈 Chat for more info")
+        st.title("💬 Stock Assistant")
+        st.session_state.messages = []
 
 
-        # Set a default model
-        if "openai_model" not in st.session_state:
-        st.session_state["openai_model"] = "gpt-3.5-turbo"
 
         # Initialize chat history
         if "messages" not in st.session_state:
-        st.session_state.messages = []
+            st.session_state.messages = []
 
         # Display chat messages from history on app rerun
         for message in st.session_state.messages:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
 
-                # Accept user input
-                if prompt := st.chat_input("What is up?"):
-                    # Add user message to chat history
-                    st.session_state.messages.append({"role": "user", "content": prompt})
-                    # Display user message in chat message container
-                    with st.chat_message("user"):
-                        st.markdown(prompt)
+        # Accept user input
+        prompt = st.chat_input("What is up?")
+        if prompt :
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            # Display user message in chat message container
+            with st.chat_message("user"):
+                st.markdown(prompt)
 
-        with st.chat_message("assistant"):
-            stream = client.chat.completions.create(
-                model=st.session_state["openai_model"],
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    for m in st.session_state.messages
-                ],
-                stream=True,
-            )
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})"""
-
+            with st.chat_message("assistant"):
+                with st.spinner("Thinking..."):
+                    print(prompt)
+                    response = bedrock_agent.ask_claude(prompt)
+                st.markdown(response)
+            
+            st.session_state.messages.append({"role": "assistant", "content": response})
