a34bf53fdd431361ebf79c1da6aecf6a18e83e25
JerPatterson <jeremie.patterson@polymtl.ca>
2024-11-03 10:23:07 -0500

    first selection in menu working to dashboard

--- main.py
+++ main.py
@@ -1,37 +1,29 @@
 import streamlit as st
-import json
-import uuid
-from datetime import datetime, timedelta
-from src.data.stock_data import StockDataAPI
-from src.visualization.dashboard import Dashboard
-from src.models import bedrock_agent
-from src.analysis.technical import TechnicalAnalysis
+from src.visualization import company
 
 
-def init_state():
-    """Initialize session state variables"""
-    st.session_state.selected = "BNC"
-    st.session_state.history = [("AMZN", "Amazon"), ("BNC", "Banque Nationale")]
+class MainPage():
+    def __init__(self):
+        self.init_state()
+        st.set_page_config(page_title="Stock Analysis Tool", layout="wide")
 
+        st.sidebar.title("History")
 
-def main():
-    init_state()
-    st.set_page_config(page_title="Stock Analysis Tool", layout="wide")
+        for symbol, company in st.session_state.history:
+            if st.sidebar.button(company):
+                self.navigate(symbol)
+                
+    
+    def init_state(self):
+        """Initialize session state variables"""
+        st.session_state.selected = "BNC"
+        st.session_state.history = [("AMZN", "Amazon"), ("BNC", "Banque Nationale")]
 
-    # Define a function to set the navigation state
-    def navigate(symbol):
+    
+    def navigate(self, symbol):
         st.session_state.selected = symbol
-
-    st.sidebar.title("History")
-
-    for symbol, company in st.session_state.history:
-        if st.sidebar.button(company):
-            navigate(symbol)
-
-        if st.session_state.selected == symbol:
-            st.title(f"Analysis of {company}")
-            st.write("Welcome to the home page!")
+        company.CompanyPage(symbol)
 
 
 if __name__ == "__main__":
-    main()
+    MainPage()
